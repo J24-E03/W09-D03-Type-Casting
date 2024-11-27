77e44ebae6f235ea145eeed5cede1e20119809bd
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2024-11-27 12:34:10 +0100

    Implement narrowing primitives

--- labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/NarrowingPrimitives.java
+++ labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/NarrowingPrimitives.java
@@ -1,8 +1,32 @@
 public class NarrowingPrimitives {
 
-  private final long bigNumber;
+    private final long bigNumber;
 
-  public NarrowingPrimitives(long bigNumber) {
+    public NarrowingPrimitives(long bigNumber) {
     this.bigNumber = bigNumber;
   }
+
+    public long getBigNumber() {
+        return Long.valueOf(this.bigNumber);
+    }
+
+    public double toDouble() {
+        return Double.valueOf(this.bigNumber);
+    }
+
+    public float toFloat() {
+        return (float) this.bigNumber/1_000;
+    }
+
+    public int toInt() {
+        return (int) (this.bigNumber/1_000_000);
+    }
+
+    public short toShort() {
+        return (short) (this.bigNumber/1_000_000_000);
+    }
+
+    public byte toByte() {
+        return (byte) (this.bigNumber/10_000_000_000L);
+    }
 }
