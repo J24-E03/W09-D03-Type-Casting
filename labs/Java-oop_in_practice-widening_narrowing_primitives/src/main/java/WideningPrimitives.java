87499841fdcdc5460c84382eea4bf191c43d34f7
Hasan <5iveatc@gmail.com>
2024-11-27 23:05:54 +0100

    hope it's correct

--- labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/WideningPrimitives.java
+++ labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/WideningPrimitives.java
@@ -5,4 +5,34 @@ public class WideningPrimitives {
   public WideningPrimitives(byte smallNumber) {
     this.smallNumber = smallNumber;
   }
+  //simply return our number as a byte
+  public byte getSmallNumber(){
+    return smallNumber;
+  }
+
+  //simply return our number as a short
+  public short toShort(){
+    return (short) smallNumber;
+  }
+
+  //simply return our number as an int
+  public int toInt(){
+    return (int) smallNumber;
+  }
+
+  //simply return our number as a long
+  public long toLong(){
+    return (long) smallNumber;
+  }
+
+  //simply return our number as a float
+  public float toFloat(){
+    return (float) smallNumber;
+  }
+
+  //simply return our number as a double
+  public double toDouble(){
+    return (double) smallNumber;
+  }
+
 }
