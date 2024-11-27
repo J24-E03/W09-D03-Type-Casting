80628f4a816e420c83862462bf40615d99b8a11d
sumayya <sumayyasafoora0@gmail.com>
2024-11-27 13:54:13 +0100

    W09-D03-Type-Casting done

--- labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/NarrowingPrimitives.java
+++ labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/NarrowingPrimitives.java
@@ -5,4 +5,34 @@ public class NarrowingPrimitives {
   public NarrowingPrimitives(long bigNumber) {
     this.bigNumber = bigNumber;
   }
+  ///simply return our number as a long
+  public long getBigNumber(){
+    return (long) bigNumber;
+  }
+
+  //simply return our number as a double
+  public double toDouble(){
+    return (double) bigNumber;
+  }
+
+  //divide our number by 1_000 and return it as a float
+  public float toFloat(){
+    return (float) bigNumber;
+  }
+
+  //divide our number by 1_000_000 and return it as an int
+  public int toInt(){
+    return (int) bigNumber;
+  }
+
+  //divide our number by 1_000_000_000 and return it as a short
+  public short toShort(){
+    return (short) bigNumber;
+  }
+
+  //divide our number by 10_000_000_000L and return it as a byte
+  public byte toByte(){
+    return (byte) bigNumber;
+  }
+
 }
--- labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/WideningPrimitives.java
+++ labs/Java-oop_in_practice-widening_narrowing_primitives/src/main/java/WideningPrimitives.java
@@ -1,8 +1,37 @@
 public class WideningPrimitives {
 
-  private final byte smallNumber;
+    private final byte smallNumber;
 
-  public WideningPrimitives(byte smallNumber) {
-    this.smallNumber = smallNumber;
+  public WideningPrimitives( byte smallNumber){
+      this.smallNumber = smallNumber;
+    }
+  //simply return our number as a byte
+  public byte getSmallNumber(){
+    return (byte) smallNumber;
   }
-}
+    //simply return our number as a short
+    public short toShort () {
+
+    return (short) smallNumber;
+    }
+    //simply return our number as an int
+    public int toInt () {
+
+    return (int) smallNumber;
+    }
+    //simply return our number as a long
+    public long toLong () {
+
+    return (long) smallNumber;
+    }
+    //simply return our number as a float
+    public float toFloat () {
+      return (float) smallNumber;
+    }
+    //simply return our number as a double
+    public double toDouble () {
+
+    return (double) smallNumber;
+    }
+  }
+
